eaab1ee8afd7862e9b68aa833e72afa12b3db983
Abdo Saleh <abdosaleh3742103@gmail.com>
2025-09-09 23:10:29 +0300

    refactor: move iterate function declaration for better readability

--- 3_JavaScript/Lab_4/Img_gallery/main.js
+++ 3_JavaScript/Lab_4/Img_gallery/main.js
@@ -7,14 +7,6 @@ const image = document.getElementsByTagName("img")[0];
 let imageIndex = 1;
 let interval;
 
-let iterate = function(){
-    imageIndex++;
-    if(imageIndex == 5){
-        imageIndex = 1;
-    }
-    image.src = `imgs/${imageIndex}.jpg`;
-}
-
 previous.addEventListener('click', () => {
     imageIndex--;
     if(imageIndex == 0){
@@ -29,11 +21,17 @@ next.addEventListener('click', () => {
 
 play.addEventListener('click', () => {
     clearInterval(interval);
-    interval = setInterval(() => {
-        iterate();
-    }, 500);
+    interval = setInterval(iterate, 500);
 });
 
 stop.addEventListener('click', () => {
     clearInterval(interval);
 });
+
+let iterate = function(){
+    imageIndex++;
+    if(imageIndex == 5){
+        imageIndex = 1;
+    }
+    image.src = `imgs/${imageIndex}.jpg`;
+}
